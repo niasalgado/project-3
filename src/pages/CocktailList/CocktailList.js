5eed803ddf346cbe290276bcb37cc42d430fec8f
niasalgado <salgado.nia@gmail.com>
2023-05-16 17:08:53 -0400

    adds Routes to App

--- src/pages/CocktailList/CocktailList.js
+++ src/pages/CocktailList/CocktailList.js
@@ -2,10 +2,10 @@ import { useEffect, useState } from 'react';
 // import { Link } from 'react-router-dom';
 
 export default function CocktailList(props) {
-const [ cocktails, setCocktails] = useState(['margarita', 'arnold palmer']);
+const [ cocktails, setCocktails] = useState([]);
 const fetchCocktails = async () => {
   try {
-    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php'); //TODO: Link API here
+    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
     const data = await response.json();
     
     setCocktails(data.drinks);
@@ -19,6 +19,7 @@ useEffect(() => {
 }, []);
 
   return (
+    // TODO: RENDER LIST OF COCKTAILS HERE
     <div className="cocktails-list">
         {cocktails.map(c => (
             <h2 key={c.idDrink}>{c.strDrink}</h2>
