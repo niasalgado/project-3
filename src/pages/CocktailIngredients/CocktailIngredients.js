e6b8fb096f6e944ce2b34cad06eca7cea8047e87
niasalgado <salgado.nia@gmail.com>
2023-05-16 18:18:11 -0400

    wip: added state and fetch functionality to CocktailIngredients

--- src/pages/CocktailIngredients/CocktailIngredients.js
+++ src/pages/CocktailIngredients/CocktailIngredients.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function CocktailIngredients() {
+
+  const {id} = useParams()
+  const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
+
+  const [cocktail, setCocktail] = useState(null);
+  const fetchCocktail = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      setCocktail(data.drinks[0]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>CocktailIngredients</div>
   )
